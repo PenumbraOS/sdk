f1279c4b34c64d19b62c8c4a69ae10cb05e10a16
Adam Gastineau <adam@agg.im>
2025-06-08 19:38:17 -0700

    Unit file installation

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 adb shell mkdir /data/local/tmp/bin
 adb push bridge_priv_rs/target/aarch64-linux-android/release/bridge_priv_rs /data/local/tmp/bin/bridge_priv
 adb shell chmod +x /data/local/tmp/bin/bridge_priv
+adb push config/pinitd/* /sdcard/penumbra/etc/pinitd/system/
 
 ./gradlew :bridge:installDebug
 ./gradlew :example:installDebug
