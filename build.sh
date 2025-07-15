9f6727aaaa9223bc5b9cae9fd617fc5937a66812
Adam Gastineau <adam@agg.im>
2025-07-15 14:51:17 -0700

    Simultaneous building with Gradle

--- build.sh
+++ build.sh
@@ -3,9 +3,4 @@ adb push config/pinitd/* /sdcard/penumbra/etc/pinitd/system/
 
 ( cd bridge-settings/react-app && npm i && npm run build:android )
 
-./gradlew :sdk:publishToMavenLocal
-./gradlew :bridge-core:installDebug
-./gradlew :bridge-system:installDebug
-./gradlew :bridge-shell:installDebug
-./gradlew :bridge-settings:installDebug
-./gradlew :example:installDebug
+./gradlew :sdk:publishToMavenLocal :bridge-core:installDebug :bridge-system:installDebug :bridge-shell:installDebug :bridge-settings:installDebug :example:installDebug
