aaad47e6a1c3a4fe09b120bb08df71e79e086436
Adam Gastineau <adam@agg.im>
2025-08-06 20:07:46 -0700

    Enable SDK services on install

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
-adb shell mkdir -p /sdcard/penumbra/etc/pinitd/system/
+adb shell mkdir -p /sdcard/penumbra/etc/pinitd/system/enabled
 adb push config/pinitd/* /sdcard/penumbra/etc/pinitd/system/
+adb shell touch /sdcard/penumbra/etc/pinitd/system/enabled/bridge_service.unit
+adb shell touch /sdcard/penumbra/etc/pinitd/system/enabled/bridge_settings.unit
+adb shell touch /sdcard/penumbra/etc/pinitd/system/enabled/bridge_shell_service.unit
+adb shell touch /sdcard/penumbra/etc/pinitd/system/enabled/bridge_system_service.unit
 
 ( cd bridge-settings/react-app && npm i && npm run build:android )
 
