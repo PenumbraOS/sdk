864e6506c834fa0235bf78d44dbf9b475dfffdad
Adam Gastineau <adam@agg.im>
2025-08-19 20:36:32 -0700

    Restore LPA patches to fix byte twiddling infinite loop

--- frida/src/index.ts
+++ frida/src/index.ts
@@ -1,10 +1,12 @@
 import { chooseLiveClasses } from "./utils/classloader";
 import { setupNetworkHooks, setDnsProvider } from "./hooks/network";
 import { setupCliCallbackHooks } from "./hooks/cli-callbacks";
+import { setupLpaByteHooks } from "./hooks/lpa-hooks";
 
 const initialize = (): void => {
   setupNetworkHooks();
   setupCliCallbackHooks();
+  setupLpaByteHooks();
 
   const dnsProviderInstances = chooseLiveClasses(
     "com.penumbraos.bridge_system.provider.DnsProvider"
